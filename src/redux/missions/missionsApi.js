709479f5e1ca44958eda328bee27bd3d30a3423c
Precious Betine <preciousbetine@gmail.com>
2023-04-11 10:33:38 +0100

    Filter API-returned data

--- src/redux/missions/missionsApi.js
+++ src/redux/missions/missionsApi.js
@@ -4,7 +4,12 @@ const apiBase = 'https://api.spacexdata.com/v3/missions';
 
 const fetchAllMissions = async () => {
   const missions = await axios.get(apiBase);
-  return missions.data;
+  const data = missions.data.map((mission) => ({
+    mission_id: mission.mission_id,
+    mission_name: mission.mission_name,
+    description: mission.description,
+  }));
+  return data;
 };
 
 export default fetchAllMissions;
