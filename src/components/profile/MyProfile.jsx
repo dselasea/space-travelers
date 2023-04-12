e48a70cb6ddc45d30b5644ca3973c48e9abb78fe
Daniel <dselasea@gmail.com>
2023-04-12 15:18:49 +0000

    Add missions to profile

--- src/components/profile/MyProfile.jsx
+++ src/components/profile/MyProfile.jsx
@@ -4,12 +4,22 @@ import './MyProfile.css';
 
 const MyProfile = () => {
   const { rockets } = useSelector((state) => state.rockets);
+  const { missions } = useSelector((state) => state.missions);
+
+  console.log(missions);
 
   return (
     <div className="p-5 m-auto profile-page">
       <div className="row">
         <div className="col-md-6 col-12">
           <h2 className="h4">My Missions</h2>
+          <div className="card my-4">
+            <ul className="list-group list-group-flush">
+              {missions.filter((mission) => mission.reserved).map((mission) => (
+                <li className="list-group-item" key={mission.mission_id}>{mission.mission_name}</li>
+              ))}
+            </ul>
+          </div>
         </div>
         <div className="col-md-6 col-12">
           <h2 className="h4">My Rockets</h2>
